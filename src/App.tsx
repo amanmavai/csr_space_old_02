079b6ce9401fb10ab49707915e5dd5ab9b9d9b3a
Aman Mavai <amanmavai@gmail.com>
2023-12-17 10:25:38 +0530

    add backup to gitignore

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,48 @@
+import { RouterProvider, createBrowserRouter, Link } from "react-router-dom";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <Home /> },
+      {
+        path: "hooks-flow",
+        lazy: () => import("./routes/hooks_flow"),
+      },
+    ],
+  },
+
+  {
+    path: "*",
+    element: <NoMatch />,
+  },
+]);
+
 function App() {
+  return <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />;
+}
+
+export default App;
+
+function NoMatch() {
   return (
-    <div className="container mx-auto">
-      <div className="text-5xl text-green-400 text-center border-b border-gray-400 p-4">Vite + React + Tailwindcss</div>
-      <div className="mt-8"></div>
+    <div className="text-lg p-4 flex flex-col gap-4 items-center">
+      <h2 className="text-orange-500">Nothing to see here!</h2>
+      <p>
+        <Link to="/" className="text-blue-400 text-3xl">
+          Back to Home Page
+        </Link>
+      </p>
     </div>
   );
 }
 
-export default App;
+function Home() {
+  return (
+    <div className="justify-center flex text-3xl text-green-500">
+      <h2>Home</h2>
+    </div>
+  );
+}
